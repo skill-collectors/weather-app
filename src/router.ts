167fe1d96f3757d1a32dcc8987a3dfac92ab4b99
Dave Burke <dburke84@gmail.com>
2020-09-24 21:22:48 -0500

    Add CSS Demo page

--- src/router.ts
+++ src/router.ts
@@ -19,5 +19,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/css-demo',
+      name: 'css-demo',
+      component: () => import(/* webpackChunkName: "css-demo" */ './views/CssDemo.vue'),
+    },
   ],
 });
