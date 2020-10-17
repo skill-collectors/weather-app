8eb8bbd5427cce686c712e9d372b7e867cfa9349
Mitch Peck <mjohnpeck@gmail.com>
2020-10-17 09:59:41 -0500

    Change to TypeScript and Class notation

--- src/views/OpenWeatherAPIPage.vue
+++ src/views/OpenWeatherAPIPage.vue
@@ -19,31 +19,32 @@
   </main>
 </template>
 
-<script>
+<script lang="ts">
 import countapi from 'countapi-js';
+import { Component, Vue } from 'vue-property-decorator';
 
-export default {
-  data() {
-    return {
-      callCount: 0,
-      output: null,
-      city: 'Minneapolis',
-    };
-  },
-  methods: {
-    getTheWeather() {
-      const url = 'https://api.openweathermap.org/data/2.5/weather?q='.concat(this.city).concat('&appid=').concat(process.env.VUE_APP_API_ACCESS_KEY);
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          this.output = data;
-          console.log(data.weather);
-        }).catch((error) => {
-          this.output = error;
-          console.log(JSON.stringify(error));
-        });
-      countapi.hit(process.env.VUE_APP_COUNTER_NAME, 'current-weather').then((result) => { this.callCount = result.value; });
-    },
-  },
-};
+@Component
+export default class ApiClass extends Vue {
+  output: any = '';
+
+  callCount: number = 0;
+
+  city: string = 'Minneapolis';
+
+  getTheWeather() {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='.concat(this.city)
+      .concat('&appid=').concat(process.env.VUE_APP_API_ACCESS_KEY);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.output = data;
+        console.log(data.weather);
+      }).catch((error) => {
+        this.output = error;
+        console.log(JSON.stringify(error));
+      });
+    countapi.hit(process.env.VUE_APP_COUNTER_NAME, 'current-weather')
+      .then((result: { value: number; }) => { this.callCount = result.value; });
+  }
+}
 </script>
