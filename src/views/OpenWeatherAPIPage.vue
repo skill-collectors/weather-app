95227fbd230e4f1d9eac5edf8584c184045da3b7
Mitch Peck <mjohnpeck@gmail.com>
2020-10-17 07:48:17 -0500

    Add JSON viewer and City input

--- src/views/OpenWeatherAPIPage.vue
+++ src/views/OpenWeatherAPIPage.vue
@@ -1,11 +1,19 @@
 <template>
   <main>
-    <b-jumbotron header="Open Weather API"></b-jumbotron>
-    <b-container>
-      <p>"Weather forecasts, nowcasts and history in fast and elegant way"</p>
-      <p><b-link href="https://openweathermap.org/">Open Weather Map </b-link></p>
-
-      <b-button v-on:click="getTheWeather()" variant="secondary">Get Weather</b-button>
+    <b-jumbotron header="Open Weather API">
+      <p><b-link href="https://openweathermap.org/">Open Weather Map</b-link> - "Weather forecasts, nowcasts and history in fast and elegant way"</p>
+    </b-jumbotron>
+    <b-container style="width:60%;margin:auto;">
+      <p>Enter a city to test out the <b-link href="https://openweathermap.org/current">Current Weather API.</b-link></p>
+  <b-row class="my-3" style="width:400px;margin:auto;">
+      <b-input-group prepend="City">
+            <b-form-input v-model="city" type="text"></b-form-input>
+      </b-input-group>
+  </b-row>
+          <b-button v-on:click="getTheWeather()" variant="secondary">Current Weather</b-button>
+      <b-card class="mt-3" header="Response Data">
+        <pre class="m-0" style="text-align:left;">{{ output }}</pre>
+      </b-card>
     </b-container>
   </main>
 </template>
@@ -13,19 +21,20 @@
 export default {
   data() {
     return {
-      city: 'minneapolis',
+      output: null,
+      city: 'Minneapolis',
     };
   },
   methods: {
     getTheWeather() {
-      const url = 'https://api.openweathermap.org/data/2.5/weather?q=minneapolis&appid='.concat(process.env.VUE_APP_API_ACCESS_KEY);
-      alert(url);
+      const url = 'https://api.openweathermap.org/data/2.5/weather?q='.concat(this.city).concat('&appid=').concat(process.env.VUE_APP_API_ACCESS_KEY);
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
-          console.log(JSON.stringify(data));
-          alert(JSON.stringify(data));
+          this.output = data;
+          console.log(data.weather);
         }).catch((error) => {
+          this.output = error;
           console.log(JSON.stringify(error));
         });
     },
