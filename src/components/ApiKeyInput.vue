e2a01cdfea397243e08dba6c5adcc4abb93ba4d6
Dave Burke <dburke84@gmail.com>
2020-12-04 21:22:25 -0600

    Add ApiKeyInput component

---
+++ src/components/ApiKeyInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-input :value="apiKey" @input="setApiKey"></b-input>
+</template>
+<script lang="ts">
+import { SET_API_KEY } from '@/store/mutations';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class OpenWeatherSample extends Vue {
+  get apiKey() {
+    return this.$store.apiKeys.openWeather;
+  }
+
+  setApiKey(newKey: string) {
+    this.$store.commit(SET_API_KEY, { apiName: 'openWeather', newKey });
+  }
+}
+</script>
