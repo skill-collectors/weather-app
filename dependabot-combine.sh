4025f93984676960015ace99a4914bb5ebc8f338
Dave Burke <dburke84@gmail.com>
2021-01-18 09:08:35 -0600

    Update dependabot-combine.sh to check all commits
    dependabot-combine.sh now checks each commit by:
    
    - running unit tests
    - running a full build
    - building storybook
    
    If any check fails, the commit is discarded.

--- dependabot-combine.sh
+++ dependabot-combine.sh
@@ -8,16 +8,30 @@ git checkout master
 git pull --all
 git checkout -b "dependabot/$(date +%F)"
 
-# Cherry-pick all remote dependabot branches. Ending with "|| true" prevents bash from exiting when cherry-pick "fails" on merge conflicts.
-git branch -r | grep dependabot | sed 's/^\s\+//' | sort | xargs git cherry-pick || true
-
-# Use mergetool until there are no more conflicts
-while [[ -n "$(git diff --name-only --diff-filter=U)" ]]; do
-	git mergetool package.json # Don't bother merging package-lock.json
-	npm install # Just regenerate package-lock.json
-	git add package.json package-lock.json
-	git cherry-pick --continue || true # Ignore conflict "failures" with '|| true'. We'll get them in the next loop.
+# This won't work if the branch name ever has spaces
+for branch in $(git branch -r | grep dependabot | sed 's/^\s\+//'); do
+	echo "Attempting to cherry-pick ${branch}"
+	git cherry-pick "${branch}" || true # don't fail the whole script on merge conflicts
+	if [[ -n "$(git diff --name-only --diff-filter=U)" ]]; then
+		echo "There were merge conflicts while cherry-picking ${branch}"
+		git mergetool package.json # Don't bother merging package-lock.json
+		npm install
+		git add package.json package-lock.json
+		git cherry-pick --continue
+	else
+		npm install # still need to do this for the test build
+	fi
+	if npm run test:unit && npm run build && npm run build-storybook; then
+		echo "Build succeeded after cherry-picking ${branch}"
+		git add package.json package-lock.json
+		git commit --amend --no-edit
+	else
+		echo "Build failed after cherry-picking ${branch}. Discarding the commit."
+		git reset --hard HEAD^
+		npm install # Reset dependencies as well
+	fi
 done
+
 rm -f package.json.orig # Remove mergetool backup file, if present.
 git log --oneline master..HEAD
 
