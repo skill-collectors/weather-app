75be9c3f65b61fb38820497c4cd11592db13a3a6
Dave Burke <dburke84@gmail.com>
2021-01-27 21:14:35 -0600

    d8781b5

---
+++ precache-manifest.54e962d1f1f4ea05e10cb2c9accc8e5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52fee05029179635f9e",
+    "url": "/weather-app/css/app.1b102099.css"
+  },
+  {
+    "revision": "ebb5da9a5b0f8ece0034",
+    "url": "/weather-app/css/chunk-vendors.5d5dbf4c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/weather-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5ac2230c3def4e941f1b203244a4c456",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a52fee05029179635f9e",
+    "url": "/weather-app/js/app.ed197c14.js"
+  },
+  {
+    "revision": "ebb5da9a5b0f8ece0034",
+    "url": "/weather-app/js/chunk-vendors.375b24da.js"
+  },
+  {
+    "revision": "e4145b409406885b284057e295c18047",
+    "url": "/weather-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-app/robots.txt"
+  }
+]);
