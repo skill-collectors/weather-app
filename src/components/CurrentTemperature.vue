61e37917e939a13e93c7c82bef6bad3495c98340
Dave Burke <dburke84@gmail.com>
2021-02-06 15:16:07 -0600

    Add stub current temp component

---
+++ src/components/CurrentTemperature.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>{{currentTemperature}}°</div>
+    <div>Feels like {{currentFeelsLike}}°</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+const CurrentTemperatureProps = Vue.extend({
+  props: {
+    currentTemperature: Number,
+    currentFeelsLike: Number,
+  },
+});
+
+@Component
+export default class CurrentTemperature extends CurrentTemperatureProps {}
+</script>
+
+<style>
+
+</style>
