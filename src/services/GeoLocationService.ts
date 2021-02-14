b8358a12889c69022769208951a5f442baf33bf7
Mitch Peck <mjohnpeck@gmail.com>
2021-02-13 18:12:08 -0600

    Update packages and version

--- src/services/GeoLocationService.ts
+++ src/services/GeoLocationService.ts
@@ -3,20 +3,17 @@ import openWeather from '@/services/openWeatherService';
 import { GeolocationCoordinates } from '@/store/types';
 import { SET_CITY, SET_LAT, SET_LON } from '../store/mutations';
 
-export default class GeoLocationService {
-  static errorMessge: string = 'Unable to retrieve your location';
+export default {
 
-  private output: object = {};
-
-  static getCurrentPosition(): Promise<GeolocationCoordinates> {
+  async getCurrentPosition(): Promise<GeolocationCoordinates> {
     return new Promise((resolve, reject) => {
       if (!navigator.geolocation) {
         reject(new Error('Geolocation is not supported by your browser'));
       }
       navigator.geolocation.getCurrentPosition(
         (position) => resolve(position.coords),
-        () => reject(new Error(this.errorMessge)),
+        () => reject(new Error('Unable to retrieve your location')),
       );
     });
-  }
-}
+  },
+};
