d6f8f639544f092f2484de0c305fddeff62915a2
Dave Burke <dburke84@gmail.com>
2021-03-06 10:11:55 -0600

    Add more granular error handling to location picker

--- src/components/BottomBar/LocationPicker.vue
+++ src/components/BottomBar/LocationPicker.vue
@@ -37,29 +37,41 @@ export default class LocationPicker extends Vue {
   }
 
   async getCurrentCity() {
-    await geolocationService.getCurrentPosition().then(async (coords) => {
+    try {
+      const coords = await geolocationService.getCurrentPosition();
       this.$store.commit(SET_LAT, coords.latitude);
       this.$store.commit(SET_LON, coords.longitude);
       // This might not be the best approach to displaying the City name and
       // the converstion from coords to city might be alittle off
       // TODO: We could remove the city setting all together and blank out
       // the city since they are searching by coordinates and may not be in a city
-      const location: GeoDirectResponse[] = await openWeatherService
-        .searchCityByCoords(coords.latitude, coords.longitude);
-      this.$store.commit(SET_CITY, { city: location[0].name });
+      try {
+        const location: GeoDirectResponse[] = await openWeatherService
+          .searchCityByCoords(coords.latitude, coords.longitude);
+        this.$store.commit(SET_CITY, { city: location[0].name });
+        try {
+          const weather: OneCallWeather = await openWeatherService
+            .getOneCallWeather(coords.latitude, coords.longitude);
+          this.$store.commit(SET_WEATHER, weather);
+        } catch (err) {
+          this.showError(`Could not load the weather for your location. ${err.message}`);
+        }
+      } catch (err) {
+        this.showError(`Could not determine your city from your location. ${err.message}`);
+      }
+    } catch (err) {
+      this.showError(`Could not retrieve your location: ${err.message}`);
+    }
+  }
 
-      const weather: OneCallWeather = await openWeatherService
-        .getOneCallWeather(coords.latitude, coords.longitude);
-      this.$store.commit(SET_WEATHER, weather);
-    }).catch((error) => {
-      this.$bvToast.toast(`I'm sorry, there seems to be an issue. Error Message: ${error.message}`,
-        {
-          title: 'Oopsy-Daisy',
-          variant: 'danger',
-          solid: true,
-          toaster: 'b-toaster-top-center',
-        });
-    });
+  showError(message: string) {
+    this.$bvToast.toast(`I'm sorry, there seems to be an issue. ${message}`,
+      {
+        title: 'Oopsy-Daisy',
+        variant: 'danger',
+        solid: true,
+        toaster: 'b-toaster-top-center',
+      });
   }
 
   async handleSearch() {
