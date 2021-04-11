ce27bd955dc822a7426466bb12c1f7b14d82360b
Dave Burke <dburke84@gmail.com>
2021-04-11 08:21:24 -0500

    Add a functional test for ComingUpList and fix rendering
    The key was adding @Component to ComingUpList.vue. Without it, the props
    never got injected and so would never render.

--- src/components/ComingUpList.vue
+++ src/components/ComingUpList.vue
@@ -1,23 +1,19 @@
 <template>
   <ul>
-    <li v-if="notifications && notifications.length === 0">Nothing to worry about!</li>
-    <li v-for="notification in notifications" :key="notification.text">
+    <li v-if="!notifications || notifications.length === 0">Nothing to worry about!</li>
+    <li v-else v-for="notification in notifications" :key="notification.text">
       <img :src="notification.iconUrl"/>{{notification.text}}
     </li>
   </ul>
 </template>
 
 <script lang="ts">
-import { Vue, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 import { ComingUpNotification } from '@/services/ComingUpService';
 
+@Component
 export default class ComingUpList extends Vue {
   @Prop({ type: Array, default: () => [] }) public notifications!: ComingUpNotification[];
-
-  constructor() {
-    super();
-    this.notifications = [];
-  }
 }
 </script>
 
--- tests/unit/components/ComingUpList.spec.ts
+++ tests/unit/components/ComingUpList.spec.ts
@@ -7,4 +7,14 @@ describe('ComingUpList', () => {
     const wrapper = mount(ComingUpList, {});
     expect(wrapper.find('ul').element).not.toBeUndefined();
   });
+  it('renders the text of a given notification', () => {
+    const wrapper = mount(ComingUpList, {
+      propsData: {
+        notifications: [
+          { type: 'RAIN', iconUrl: '', text: 'expected' },
+        ],
+      },
+    });
+    expect(wrapper.find('li').text()).toMatch(/expected/);
+  });
 });
