ea8281e9e22dae9a02ed29d7590245dfc1a8225c
Dave Burke <dburke84@gmail.com>
2023-04-19 20:32:04 -0500

    Fix PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,35 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        "name": "weather-app",
+        "short_name": "weather-app",
+        "icons": [
+          {
+            "src": "./img/icons/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "./img/icons/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        "start_url": "./index.html",
+        "display": "standalone",
+        "background_color": "#000000",
+        "theme_color": "#FFFFFF"
+      }
+    }
+  )],
   base: process.env.NODE_ENV === 'production'
       ? '/weather-app/'
       : '/',
