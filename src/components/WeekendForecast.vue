2033615636380e661253a3ca0bca0ae48ef02c0a
Dave Burke <dburke84@gmail.com>
2025-05-30 16:37:26 -0500

    Fill out weekend view details

--- src/components/WeekendForecast.vue
+++ src/components/WeekendForecast.vue
@@ -5,6 +5,7 @@ import { useStore } from '@/store/store'
 import { computed } from 'vue'
 
 const formatDay = (dt) => format(convert.dtToDate(dt), 'EEEE')
+const formatTime = (dt) => format(convert.dtToDate(dt), 'h:mm')
 const iconToUrl = convert.iconToUrl
 
 const store = useStore()
@@ -21,14 +22,81 @@ const days = computed(() => {
 </script>
 <template>
   <h4>This weekend</h4>
-  <v-card v-for="day in days" :key="day.dt" class="my-2">
+  <v-card v-for="day in days" :key="day.dt" class="my-4">
     <template #prepend>
-      <img :src="iconToUrl(day.weather[0].icon)" />
       {{ formatDay(day.dt) }}
     </template>
-    <v-card-title> {{ Math.round(day.temp.max) }}° / {{ Math.round(day.temp.min) }}° </v-card-title>
-    <v-card-text>
+    <template #append>
+      <img :src="iconToUrl(day.weather[0].icon)" />
+      <v-card-title>
+        {{ Math.round(day.temp.max) }}° / {{ Math.round(day.temp.min) }}°
+      </v-card-title>
+    </template>
+    <v-card-subtitle>
       {{ day.summary }}
+    </v-card-subtitle>
+    <v-card-text>
+      <v-row>
+        <v-col>
+          <v-icon>mdi-weather-sunset-up</v-icon>
+          Sunrise: <strong>{{ formatTime(day.sunrise) }}</strong>
+        </v-col>
+        <v-col>
+          <v-icon>mdi-weather-sunset-down</v-icon>
+          Sunset: <strong>{{ formatTime(day.sunset) }}</strong>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-icon v-if="day.uvi > 6">mdi-weather-sunny-alert</v-icon>
+          <v-icon v-else>mdi-weather-sunny</v-icon>
+          UV Index: <strong>{{ Math.round(day.uvi) }}</strong>
+        </v-col>
+        <v-col>
+          <v-icon :style="`transform: rotate(${day.wind_deg}deg)`">mdi-arrow-up</v-icon>
+          Wind: <strong>{{ Math.round(day.wind_speed) }}</strong>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-icon>mdi-water-percent</v-icon>
+          Humidity: <strong>{{ day.humidity }}</strong>
+        </v-col>
+        <v-col>
+          <v-icon>mdi-thermometer-water</v-icon>
+          Dew point: <strong>{{ Math.round(day.dew_point) }}</strong>
+        </v-col>
+      </v-row>
+      <v-table density="compact" class="text-caption my-2">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Morning</th>
+            <th>Afternoon</th>
+            <th>Evening</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Temp</td>
+            <td>{{ Math.round(day.temp.morn) }}°</td>
+            <td>{{ Math.round(day.temp.day) }}°</td>
+            <td>{{ Math.round(day.temp.eve) }}°</td>
+          </tr>
+          <tr>
+            <td>Feels like</td>
+            <td>{{ Math.round(day.feels_like.morn) }}°</td>
+            <td>{{ Math.round(day.feels_like.day) }}°</td>
+            <td>{{ Math.round(day.feels_like.eve) }}°</td>
+          </tr>
+        </tbody>
+      </v-table>
     </v-card-text>
   </v-card>
 </template>
+<style scoped>
+td,
+th {
+  padding: 0 !important;
+}
+</style>
