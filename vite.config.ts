a51a8e283a43c1226ddad5782c05b431c7288d26
Dave Burke <dburke84@gmail.com>
2025-06-03 16:06:09 -0500

    Update manifest and icons

--- vite.config.ts
+++ vite.config.ts
@@ -6,37 +6,45 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
       manifest: {
-        "name": "weather-app",
-        "short_name": "weather-app",
-        "icons": [
+        name: 'weather-app',
+        background_color: '#000000',
+        theme_color: '#FFFFFF',
+        short_name: 'weather-app',
+        icons: [
           {
-            "src": "./img/icons/android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
-        ],
-        "start_url": "./index.html",
-        "display": "standalone",
-        "background_color": "#000000",
-        "theme_color": "#FFFFFF"
+        ]
       }
-    }
-  )],
-  base: process.env.NODE_ENV === 'production'
-      ? '/weather-app/'
-      : '/',
+    })
+  ],
+  base: process.env.NODE_ENV === 'production' ? '/weather-app/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
+  }
 })
